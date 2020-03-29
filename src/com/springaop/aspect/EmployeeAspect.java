a005183e058d7322c80ed832360bea9c6505951e
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-03-30 01:49:14 +0530

    [Rupesh-Patil].added Aspect in Spring Aop Example

---
+++ src/com/springaop/aspect/EmployeeAspect.java
@@ -0,0 +1,17 @@
+package com.springaop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class EmployeeAspect {
+	@Before("execution(public String getName())")
+	public void getNameAdvice() {
+		System.out.println("Executing Advice on getName()");
+	}
+
+	@Before("execution(* com.springaop.service.*.get*())")
+	public void getAllAdvice() {
+		System.out.println("Service method getter called");
+	}
+}
